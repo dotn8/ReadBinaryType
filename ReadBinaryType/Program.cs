a076b5fbe08d63587fad2746d6e9b38e01769f88
dotn8 <dotn8@users.noreply.github.com>
2014-06-23 14:52:01 -0500

    Fix a bug where the return value wasn't getting returned but rather was getting written to standard output. (Wouldn't compile)

--- ReadBinaryType/Program.cs
+++ ReadBinaryType/Program.cs
@@ -24,23 +24,19 @@ namespace ReadBinaryType
             switch (binaryType)
             {
                 case BinaryType.SCS_32BIT_BINARY:
-                    Console.WriteLine("32-bit Windows-based application,\t{0}", fileName);
-                    break;
+                    return String.Format("32-bit Windows-based application,\t{0}", fileName);
                 case BinaryType.SCS_64BIT_BINARY:
-                    Console.WriteLine("64-bit Windows-based application,\t{0}", fileName);
-                    break;
+                    return String.Format("64-bit Windows-based application,\t{0}", fileName);
                 case BinaryType.SCS_DOS_BINARY:
-                    Console.WriteLine("MS-DOS-based application,\t{0}", fileName);
-                    break;
+                    return String.Format("MS-DOS-based application,\t{0}", fileName);
                 case BinaryType.SCS_OS216_BINARY:
-                    Console.WriteLine("16-bit OS/2-based application,\t{0}", fileName);
-                    break;
+                    return String.Format("16-bit OS/2-based application,\t{0}", fileName);
                 case BinaryType.SCS_POSIX_BINARY:
-                    Console.WriteLine("POSIX-based application,\t{0}", fileName);
-                    break;
+                    return String.Format("POSIX-based application,\t{0}", fileName);
                 case BinaryType.SCS_WOW_BINARY:
-                    Console.WriteLine("16-bit Windows-based application,\t{0}", fileName);
-                    break;
+                    return String.Format("16-bit Windows-based application,\t{0}", fileName);
+                default:
+                    return "Unknown";
             }
         }
     }
